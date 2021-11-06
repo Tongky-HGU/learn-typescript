d71c615b0b74e5d8a0303ba196ffc0966db1a9b7
chosh <chotjd329@hotmail.com>
2021-11-06 20:57:19 +0900

    ✨ 타입
    alias와의 차이는 확장

---
+++ class-note/4_type-aliases.ts
@@ -0,0 +1,26 @@
+// interface Person {
+//     name:string,
+//     age:number
+// }
+
+type Person2 =  {
+     name:string,
+    age:number
+}
+
+let seho2: Person2={
+    name: '세호',
+    age:30
+}
+
+type MyString = string;
+const str: MyString = "hello"
+
+type MyTodo = {name:string, age : number}
+function setTodo  (todo:MyTodo){}
+
+
+//! type vs interface
+//* 프리뷰 상태를 볼 수 있다. 
+
+//* 타입은 확장을 할 수 없다.
