05640a86e2cda336d55a43046cbd3660a64ffe5a
chosh <chotjd329@hotmail.com>
2021-12-22 22:56:52 +0900

    ✨ 유틸리티함수

--- class-note/13_utility_type.ts
+++ class-note/13_utility_type.ts
@@ -6,7 +6,7 @@ interface Product {
   stock: number;
 }
 
-// 상품 목록을 받아오기 위한 api 핫ㅁ수
+// 상품 목록을 받아오기 위한 api 함수
 function fetchProducts(): Promise<Product[]> {
   return;
   // ..
@@ -18,8 +18,44 @@ function fetchProducts(): Promise<Product[]> {
 //   price: number;
 // }
 
+// 2. 특정 상품의 상세정보를 나타내기 위한 함수
 type ShoppingItem = Pick<Product, "id" | "name" | "price">;
 
 function displayProductDetail(
   shoppingItem: Pick<Product, "id" | "name" | "price">
 ) {}
+
+// 3. 특정 상품 정보를 업데이트(갱신)하는 함수
+type UpdateProduct = Partial<Product>;
+
+function updateProduce(productItem: UpdateProduct) {}
+
+// 4.  유틸리티 타입 구현하기 - Partial
+interface UserProfile {
+  username: string;
+  email: string;
+  profilePhotoUrl: string;
+}
+
+// #1
+// type UserProfileUpdate = {
+//   username?: UserProfile["username"];
+//   email?: UserProfile["email"];
+//   profilePhotoUrl?: UserProfile["profilePhotoUrl"];
+// };
+
+// #2
+// type UserProfileUpdate = {
+//   [p in "username" | "email" | "profilePhotoUrl"]: UserProfile[p];  // mapped type;
+// };
+
+type userProfileKeys = keyof UserProfile;
+
+// #3
+type UserProfileUpdate = {
+  [p in keyof UserProfile]?: UserProfile[p];
+};
+
+type Subset<T> = {
+  [p in keyof T]?: T[p];
+};
